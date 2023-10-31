7abd6087ce6e91258eab4f60a0b7db1e25ed2f38
beastrobel <beatrizstrobel@proton.me>
2023-10-30 20:00:26 -0400

    Added JQuery so that I can use the remove() function. It also simplifies the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,23 +7,27 @@
 //<button>3. </button>  
 //<button>4. </button>  
 
-var timer = document.getElementById('timer');
-var quiz  = document.getElementById('quiz');
+//Wraps code in JQuery function
+$(function(){
+
+var timer = $('#timer');
+var quiz  = $('#quiz');
 
 //Appends start page
-var startScreen = quiz.appendChild(document.createElement("h2"));
-startScreen.innerHTML = 'Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!';
-var startButton = quiz.appendChild(document.createElement("button"));
-startButton.setAttribute("id", "start");
-startButton.innerHTML = 'Start Quiz';
+var startScreen = $("<h2></h2>").text('Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!');
+var startButton = $("<button></button>").text('Start Quiz');
+startButton.attr('id', 'start');
+$('#quiz').append(startScreen, startButton);
 
-//Timer - Based on class example 
+startButton.on("click", countdown, remove);
+
+//Timer - Based on class example, modified for this project
 function countdown() {
     var timeLeft = 75;
     var timeInterval = setInterval(function () {
       timeLeft--;
-      timer.textContent = timeLeft;
-  
+      timer.textContent = 'Time: ' + timeLeft;
+
       if(timeLeft <= 0) {
         // Stops execution of action at set interval
         clearInterval(timeInterval);
@@ -32,12 +36,17 @@ function countdown() {
         displayMessage();
       }
     }, 1000);
-  }
+}
+
+function remove() {
+    //Removes start screen
+    $('h2').remove();
+    $('button').remove();
+}
 
 function displayMessage() {
     console.log ('Success!');
 }
 
 
-
-countdown();
+});
